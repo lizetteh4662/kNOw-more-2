b032b2f7fed9ca9a4a4e3b05c505e58c07ebea2b
Malia Salote Vakauta <maliasvakauta@fb.com>
2020-07-28 20:00:32 -0700

    added print statement "Hi I am here" under override func viewDidLoad on ViewController

--- kNOw more/ViewController.swift
+++ kNOw more/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+         print("Hi I am here")
     }
 
     @IBAction func TopicPressed(_ sender: Any) {
